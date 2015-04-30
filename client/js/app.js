93c7f44b935f325bff42c34f0719739929ccf8c9
BenLeroy <bleroy@live.fr>
2015-04-30 16:26:09 +0200

    Updated views

--- client/js/app.js
+++ client/js/app.js
@@ -2,27 +2,40 @@
 
 (function() {
 
-  var app = angular.module('userList', ['lbServices', 'ui.router']);
+  var app = angular.module('userList'
+                          , ['lbServices'
+                          , 'ui.router']);
+
+  app.config(
+      ['$stateProvider'
+      , '$urlRouterProvider'
+      , function ($stateProvider, $urlRouterProvider) {
+
+        $stateProvider.state('liste', {
+          url: 'liste'
+          , templateUrl: 'views/liste.html'
+          //, controller: 'ListController'
+        })
+        .state('index', {
+          url: ''
+          , template: '<h1>Index</h1><br/>'
+        });
+        $urlRouterProvider.otherwise('index');
+
+      }]);
 
 
-  app.controller('ListController', function (Dena_User){
-    var List = this;
+  app.controller('ListController'
+    , function (Dena_User){
 
-    List.users = [];
+      var List = this;
 
-    Dena_User.find().$promise.then(function(data){
+      List.users = [];
 
-      //console.log(data);
-      List.users = data;
+      Dena_User.find().$promise.then(function(data){
+
+        List.users = data;
+      });
     });
 
-    app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-      $stateProvider.state('liste', {
-        url: '',
-        templateUrl: 'views/liste.html',
-        //controller: 'ListController'
-      });
-       $urlRouterProvider.otherwise('liste');
-    }]);
-  });
 })();
