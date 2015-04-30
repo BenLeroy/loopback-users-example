f4ac612d47e626f674ad80b88bd187f137f46cba
BenLeroy <bleroy@live.fr>
2015-04-30 15:05:57 +0200

    Added ui-router logic (NW)

--- client/js/app.js
+++ client/js/app.js
@@ -2,7 +2,7 @@
 
 (function() {
 
-  var app = angular.module('userList', ['lbServices']);
+  var app = angular.module('userList', ['lbServices', 'ui.router']);
 
 
   app.controller('ListController', function (Dena_User){
@@ -12,8 +12,17 @@
 
     Dena_User.find().$promise.then(function(data){
 
-      console.log(data);
+      //console.log(data);
       List.users = data;
     });
+
+    app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+      $stateProvider.state('liste', {
+        url: '',
+        templateUrl: 'views/liste.html',
+        //controller: 'ListController'
+      });
+       $urlRouterProvider.otherwise('liste');
+    }]);
   });
 })();
